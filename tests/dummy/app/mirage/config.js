f08ef5eb6f7a8f3396c31fb4f34b30b2ffc82f9f
Zach Garwood <zachgarwood@gmail.com>
2016-05-19 22:55:42 -0500

    Created Mirage routes and factories

--- tests/dummy/app/mirage/config.js
+++ tests/dummy/app/mirage/config.js
@@ -1,82 +1,45 @@
+import ENV from '../../config/environment';
+
 export default function() {
 
-  // These comments are here to help you get started. Feel free to delete them.
+  this.urlPrefix = ENV.socrata.dataRepo;
+  this.namespace = 'resource';
 
-  /*
-    Config (with defaults).
+  // companies
+  this.get('/abcd-1234.json', function(db, request) {
+    let results = [];
+    if ('$where' in request.queryParams) {
+      let query = request.queryParams['$where'].split(' = ');
+      results = this._queryForItems(db.company, query);
+    } else {
+      results = db.company;
+    }
 
-    Note: these only affect routes defined *after* them!
-  */
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+    return results;
+  });
 
-  /*
-    Route shorthand cheatsheet
-  */
-  /*
-    GET shorthands
+  // employees
+  this.get('/wxyz-7890.json', function(db, request) {
+    let results = [];
+    if ('$where' in request.queryParams) {
+      let query = request.queryParams['$where'].split(' = ');
+      results = this._queryForItems(db.employee, query);
+    } else {
+      results = db.employee;
+    }
 
-    // Collections
-    this.get('/contacts');
-    this.get('/contacts', 'users');
-    this.get('/contacts', ['contacts', 'addresses']);
+    return results;
+  });
 
-    // Single objects
-    this.get('/contacts/:id');
-    this.get('/contacts/:id', 'user');
-    this.get('/contacts/:id', ['contact', 'addresses']);
-  */
-
-  /*
-    POST shorthands
-
-    this.post('/contacts');
-    this.post('/contacts', 'user'); // specify the type of resource to be created
-  */
-
-  /*
-    PUT shorthands
-
-    this.put('/contacts/:id');
-    this.put('/contacts/:id', 'user'); // specify the type of resource to be updated
-  */
-
-  /*
-    DELETE shorthands
-
-    this.del('/contacts/:id');
-    this.del('/contacts/:id', 'user'); // specify the type of resource to be deleted
-
-    // Single object + related resources. Make sure parent resource is first.
-    this.del('/contacts/:id', ['contact', 'addresses']);
-  */
-
-  /*
-    Function fallback. Manipulate data in the db via
-
-      - db.{collection}
-      - db.{collection}.find(id)
-      - db.{collection}.where(query)
-      - db.{collection}.update(target, attrs)
-      - db.{collection}.remove(target)
-
-    // Example: return a single object with related models
-    this.get('/contacts/:id', function(db, request) {
-      var contactId = +request.params.id;
-
-      return {
-        contact: db.contacts.find(contactId),
-        addresses: db.addresses.where({contact_id: contactId})
-      };
+  this._queryForItems = function(collection, query) {
+    let results = [];
+    collection.forEach(function(model) {
+      for (let attribute in model) {
+        if (attribute === query[0] && model[attribute] === query[1]) {
+          results.push(model);
+        }
+      }
     });
-
-  */
+    return results;
+  };
 }
-
-/*
-You can optionally export a config that is only loaded during tests
-export function testConfig() {
-
-}
-*/
