a888b7d87cf58a39e112487499a170e261b38ca0
Zach Garwood <zachgarwood@gmail.com>
2016-05-20 23:45:19 -0500

    Made serializer more unit testable

--- addon/serializers/socrata.js
+++ addon/serializers/socrata.js
@@ -6,20 +6,26 @@ export default JSONSerializer.extend({
     return resourceHash[`${Ember.String.underscore(modelClass.modelName)}_id`];
   },
   extractAttributes(modelClass, resourceHash) {
+    this._camelizeAttributes(modelClass, resourceHash); 
+    return this._super.apply(this, arguments);
+  },
+  keyForRelationship(key) {
+    return `${Ember.String.underscore(key)}_id`;
+  },
+  normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
+    return this._super(store, primaryModelClass, payload[0], id, requestType);
+  },
+
+  _camelizeAttributes(modelClass, resourceHash) {
     for (let attribute in resourceHash) {
-      let camelizedAttribute = attribute.endsWith('_id') ? attribute : Ember.String.camelize(attribute);
+      let camelizedAttribute =
+        attribute.endsWith('_id') ? attribute : Ember.String.camelize(attribute);
       if (attribute !== camelizedAttribute) {
         resourceHash[Ember.String.camelize(attribute)] = resourceHash[attribute];
         delete resourceHash[attribute];
       }
     }
-    
-    return this._super.apply(this, arguments);
-  },
-  keyForRelationship(key) {
-    return `${key}_id`;
-  },
-  normalizeSingleResponse(store, primaryModelClass, payload, id, requestType) {
-    return this._super(store, primaryModelClass, payload[0], id, requestType);
+
+    return resourceHash;
   },
 });
