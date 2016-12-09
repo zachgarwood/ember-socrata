e18df1015baa9dc4d37db8eda45e8147e0a758d6
Zach Garwood <zachgarwood@gmail.com>
2016-12-09 10:45:37 -0600

    Upgraded ember-cli to 2.10.0

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,13 +4,17 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     socrata: { dataRepo: 'some.data-repository.com' },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
@@ -30,7 +34,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
